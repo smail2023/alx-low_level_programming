f21f54c6b33f379f9ed5cbab3355f3aa57990ede
smail2023 <smailbaroda@gmail.com>
2023-07-14 19:58:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: a
  * Return: Always 0.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 int fd;
 int len = 0;
