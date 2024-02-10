1fa64b235728a2e6565861b5d7e17541d5961b0d
smail2023 <smailbaroda@gmail.com>
2024-02-10 05:41:00 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - dynamically allocates memory using malloc
+ * @b: the number of bytes to allocate
+ * Return: On success, a pointer to the allocated memory.
+ *         On failure, exits with status 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *mem_ptr = malloc(b);
+if (mem_ptr == NULL)
+exit(98);
+return (mem_ptr);
+}
