26c5b97c41f31dd85d2efdda52e51ba18a0f8310
smail2023 <smailbaroda@gmail.com>
2024-02-13 07:12:25 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,51 +1,56 @@
 #include "3-calc.h"
+
 /**
- * op_add - returns the sum of a and b
- * @a: first integer
- * @b: second integer
- * Return: sum of a and b
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
  */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
+
 /**
- * op_sub - returns the difference of a and b
- * @a: first integer
- * @b: second integer
- * Return: difference of a and b
+ * op_sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
  */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
+
 /**
- * op_mul - returns the product of a and b
- * @a: first integer
- * @b: second integer
- * Return: product of a and b
+ * op_mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
  */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
+
 /**
- * op_div - returns the result of the division of a by b
- * @a: first integer
- * @b: second integer
- * Return: division of a by b
+ * op_div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
  */
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
+
 /**
- * op_mod - returns the remainder of the division of a by b
- * @a: first integer
- * @b: second integer
- * Return: remainder of the division of a by b
+ * op_mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
  */
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
